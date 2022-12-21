fdd0f7d98c73936f2d2b41b4e0597d8debe5437f
unknown <penpenetai@icloud.com>
2022-12-21 11:00:24 -0800

    lkj2

--- src/datas/supabaseCliant.js
+++ src/datas/supabaseCliant.js
@@ -1,6 +1,6 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseURL = "https://oeyuuyjjgdpmstwwahht.supabase.co"
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL
 const supabaseAPIKey = import.meta.env.VITE_SUPABASE_APIKEY
 
 export const supabase = createClient(supabaseURL, supabaseAPIKey);
