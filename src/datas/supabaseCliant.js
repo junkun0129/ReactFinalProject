3a9513cf96b916e66e6957aee6f176298d1561e7
unknown <penpenetai@icloud.com>
2022-12-21 10:34:23 -0800

    ;lkj

--- src/datas/supabaseCliant.js
+++ src/datas/supabaseCliant.js
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseURL = import.meta.env.VITE_SUPABASE_URL
-const supabaseAPIKey = import.meta.env.VITE_SUPABASE_APIKEY
+const supabaseURL = "https://oeyuuyjjgdpmstwwahht.supabase.co"
+const supabaseAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9leXV1eWpqZ2RwbXN0d3dhaGh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA0NDg4MTIsImV4cCI6MTk4NjAyNDgxMn0.DmJqaRXgbcPpweek7n20RI5vgtoopUt-Z03rRPV0poU"
 
 export const supabase = createClient(supabaseURL, supabaseAPIKey);
 
